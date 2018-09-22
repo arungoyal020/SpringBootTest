f6287a42db619a405efd4dc5149b03f1be7c75e6
Peter <Peter@192.168.0.5>
2018-09-22 19:53:17 +0530

    adding test case

---
+++ src/test/java/MainCofigTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainCofigTest {
+
+	
+	@Test
+	public void test1()
+	{
+		assertTrue("should be'true'", true);
+	}
+	
+}
