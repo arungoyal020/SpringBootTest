09dc8ea31894daab46e112fefe8e17cf154ef079
Peter <Peter@192.168.0.5>
2018-09-22 19:06:38 +0530

    small change

--- Gradle-SpringBoot/src/main/java/com/gradle/springboot/HelloWorldController.java
+++ Gradle-SpringBoot/src/main/java/com/gradle/springboot/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.gradle.springboot;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +13,13 @@ public class HelloWorldController {
 	public String getHello()
 	{
 		return "Hello World";
+	}
+	
+	
+	@RequestMapping("/countries")
+	public List<String> getAllCountries(){
+		
+		return Arrays.asList("India","US","England","China");
 	}
 	
 	
