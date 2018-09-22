8d690809a174a0400561bcb3750c82fc3c51ba5e
Peter <Peter@192.168.0.5>
2018-09-22 20:34:15 +0530

    adding test

---
+++ src/test/java/HelloWorldControllerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloWorldControllerTest {
+	
+	@Test
+	public void test3(){
+		
+		assertTrue("test3" , true);
+	}
+
+}
